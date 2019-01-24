1be38ca8c58095b1e2a9f68410613c2aae9bcda2
Myroslav <46872445+csmerik@users.noreply.github.com>
2019-01-24 19:28:53 +0200

    commin5

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var pictures
 var request = new XMLHttpRequest ()
 request.open ("GET", "sample.json")
-request.orload = function(event){
- 
+request.onload = function (event) {
+
   if (this.status ===200) {
-    pictures = JSON.parse (this.responseText) 
+    pictures = JSON.parse (this.responseText)
     for (var url of pictures){
-      var image = document.createElement("img")
+      var image = document.createElement ("img")
       document.body.appendChild (image)
       image.src = url
       image.height = 100
     }
   }
-  
-}
+    
+} 
 request.send ()
