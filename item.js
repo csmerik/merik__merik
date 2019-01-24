b9c07cf765c718ea1de445d85b77792f86225281
Myroslav <46872445+csmerik@users.noreply.github.com>
2019-01-24 19:56:00 +0200

    commit5

---
+++ item.js
@@ -0,0 +1,13 @@
+var transport = new XMLHttpRequest ()
+transport.open (
+  "GET",
+  "sample.html"
+ )
+ transport.onload = function (event) {
+    if (this.status === 200){
+      var container = document.createElement ("main")
+      document.body.apendChild (container)
+      container.innerHTML = this.responceText
+    }
+ }
+ transport.send
